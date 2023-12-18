026927a84747c04e2010c5cb94cbc823ca922384
rusgyn <rusgynmagsanay@gmail.com>
2023-12-18 03:59:09 -0500

    modify to use console.log to display the output.

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,9 @@
 const assertEqual = function(actual, expected) {
-
-  if (actual == expected){
-    console.assert(true, actual + " === " + expected);
+  if (actual === expected){
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.assert(false, actual + " !== " + expected);
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
