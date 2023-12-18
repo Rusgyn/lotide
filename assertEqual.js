84886ff2e4400f817af1ee9d7e099ec5c9b224ed
rusgyn <rusgynmagsanay@gmail.com>
2023-12-18 04:39:01 -0500

    Swap the emoji between Assertion Passed and Assertion Failed.

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected){
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
