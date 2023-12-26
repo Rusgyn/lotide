7941846ff410bbb55485f2aab4f97a42c25f1f5b
rusgyn <rusgynmagsanay@gmail.com>
2023-12-26 17:18:31 -0500

    Function that will compare two arrays and console log the appropriate message.

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (const [idx, el] of array1.entries()) {
+    if (el !== array2[idx]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual([], []);
+assertArraysEqual(["1", "2", "3"], []);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
