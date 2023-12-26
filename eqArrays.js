686db562181eb2c4a84dafac6141041be5867a8b
rusgyn <rusgynmagsanay@gmail.com>
2023-12-26 01:16:42 -0500

    Modified the code to meet the boolean condition.

--- eqArrays.js
+++ eqArrays.js
@@ -23,6 +23,6 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
