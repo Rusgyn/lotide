cec79c67fc198f8c5493b69805fa86c374799767
rusgyn <rusgynmagsanay@gmail.com>
2023-12-26 17:25:53 -0500

    Modified the looping to regular looping to iterate over the array elements instead of using for...of.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,8 +4,8 @@ const eqArrays = function(array1, array2) {
     return false;
   }
 
-  for (const [idx, el] of array1.entries()) {
-    if (el !== array2[idx]) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
