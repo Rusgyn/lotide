5df9862eaad49f1987d2daf1aea0fc47d5fa0614
rusgyn <rusgynmagsanay@gmail.com>
2023-12-27 17:37:58 -0500

    Added emoty condition, and use assertArraysEqual() function to test the output of without() function.

--- without.js
+++ without.js
@@ -27,6 +27,11 @@ const assertArraysEqual = function(arr1, arr2) {
 // This function will return a new array with only those elements from source that are not present in the itemsToRemove array.
 const without = function(source, itemsToRemove) {
   let itemsRemain = [];
+
+  if (source.length === 0 || itemsToRemove.length === 0) {
+    return itemsRemain;
+  }
+
   for (let j = 0; j < source.length; j++) {
     if (!itemsToRemove.includes(source[j])) {
       itemsRemain.push(source[j]);
@@ -35,19 +40,20 @@ const without = function(source, itemsToRemove) {
   return itemsRemain;
 };
 
-//console.log(without([1, 2, 3], [1])); // => [2, 3]
-
 const words = ["hello", "world", "lighthouse"]; //array of string(word) typeof.
 const numbers = [1, 2, 3]; // array of number typeof.
 const numString = ["1", "2", "3"]; //array of string(number) typeof.
+const empty = []; //empty array
 
 //to test the without() by removing the value that is also existing in the given variable elements.
-console.log(without(words, ["lighthouse"]));
-console.log(without(numbers, [1]));
-console.log(without(numString, [1, 2, "3"]));
+without(words, ["lighthouse"]);
+without(numbers, [1]);
+without(numString, [1, 2, "3"]);
+without(empty,[1, "a"]);
 
 //to test using assertArraysEqual() if it will passed or failed with the different type of arrays assign to a variable.
 //Make sure the original array was not altered by the without function.
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-assertArraysEqual(numbers, [1, 2, 3]);
-assertArraysEqual(numString, ["1", "2", "3"]);
+assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
+assertArraysEqual(without(numbers, [1]), [2, 3]);
+assertArraysEqual(without(numString, [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without(empty,[1, "a"]), []);
