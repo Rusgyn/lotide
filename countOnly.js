9e3a0fbeedb3c6f17eb541fc3ebd455ec79a2f12
rusgyn <rusgynmagsanay@gmail.com>
2024-01-06 16:28:26 -0500

    Initial commit. Given an array and an object, this code will return an object containing counts of everything that the input object listed.

---
+++ countOnly.js
@@ -0,0 +1,69 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  //loop over the allItems
+  for (const item of allItems) {
+    // inside the loop,
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
+    if (itemsToCount[item]) { //Only count the specific items
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return ("The results: ", results);
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+//Test case.
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+//to test using assertEqual() if the return results has passed or failed.
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+
+/*ALTERNATIVE SOLUTION:
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      counted[item] ||= 0; //initialize to zero if undefined
+      counted[item]++;
+    }
+  }
+
+  return results;
+};
+*/
