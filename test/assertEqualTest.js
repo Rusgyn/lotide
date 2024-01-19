712a3004823a7f1a9ba7f9ec4c9c498aca5f0eb0
rusgyn <rusgynmagsanay@gmail.com>
2024-01-19 17:04:50 -0500

    Added new folder, separating the tests.

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+//test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+//TEST CODE
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
---
+++ test/headTest.js
@@ -0,0 +1,9 @@
+//test/headTest.js
+
+const head = require('../head');
+
+//TEST CODE
+
+
+head([5,2,3], 5);
+head(["Hello", "Lighthouse", "Labs"], "Hello1");
