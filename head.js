092a2c8bb9c6daea89bc6491a861551fc13134c2
rusgyn <rusgynmagsanay@gmail.com>
2024-01-19 17:15:11 -0500

    Refactor the code, added Module and Require.

--- head.js
+++ head.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
-  let headOfArray = (array && array.length ? array[0] : undefined);
-  return headOfArray;
+  return array[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello1");
+
+module.exports = head;
