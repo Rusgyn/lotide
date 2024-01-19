97b607ec0efb2ea66c16f6e178e8e1ba68c5f035
rusgyn <rusgynmagsanay@gmail.com>
2024-01-19 17:20:41 -0500

    Modify and added the required code to assert.

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,9 @@
 //test/headTest.js
-
+const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 //TEST CODE
 
 
-head([5,2,3], 5);
-head(["Hello", "Lighthouse", "Labs"], "Hello1");
+assertEqual(head([5,2,3]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello1");
