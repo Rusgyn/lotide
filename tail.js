b406cf88d264b69e8fe4e258493a6e8e09bff256
rusgyn <rusgynmagsanay@gmail.com>
2024-01-19 21:39:19 -0500

    Added module and exported the function.

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// A function tail which returns the "tail" of an array: everything except for the first item (head) of the provided array.
 
 const tail = function(array) {
   if (array === undefined || array.length === 0) {
@@ -12,21 +6,7 @@ const tail = function(array) {
   }
 
   return array.slice(1);
+  
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("1", 1);
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// assertEqual(result, ["Lighthouse", "Labs"]);
-
-assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
