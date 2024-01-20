55a9bb63d64f5b8a9d4d690d9fc3b5f680a1afcb
rusgyn <rusgynmagsanay@gmail.com>
2024-01-19 23:04:17 -0500

    Added module and exported the function.

--- middle.js
+++ middle.js
@@ -1,30 +1,5 @@
 // A function which will take in an array and return the middle-most element(s) of the given array.
 
-// This function checks if two arrays have the same elements in the same order.
-// const eqArrays = function(array1, array2) {
-
-//   if (array1.length !== array2.length) {
-//     return false;
-//   }
-
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] !== array2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// This function uses eqArrays to check if two arrays are equal and prints a message to the console.
-// const assertArraysEqual = function(arr1, arr2) {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-//   }
-// };
-
-//This function will check what is the middle elements of the given array.
 const middle = function(whatIsMidArray) {
   
   let middleOfTheArray = [];
