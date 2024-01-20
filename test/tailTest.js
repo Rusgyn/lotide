417a1838a51e0a1dd76e86dc1bbabd537deb9598
rusgyn <rusgynmagsanay@gmail.com>
2024-01-19 21:50:59 -0500

    Removed the commented out code.

--- test/tailTest.js
+++ test/tailTest.js
@@ -9,7 +9,6 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// assertEqual(result, ["Lighthouse", "Labs"]);
 
 assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
 assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
