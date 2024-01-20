e3b52e481464ed153136b49f00071715ceda0a9a
rusgyn <rusgynmagsanay@gmail.com>
2024-01-19 22:47:08 -0500

    Added require eqArrays, module and exported the functions.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,32 +1,15 @@
-/* Implement a function which will take in two arrays and console.log an appropriate message to the console. */
+// A function which will take in two arrays and console.log an appropriate message to the console.
 
-// This function checks if two arrays have the same elements in the same order.
-const eqArrays = function(array1, array2) {
+const eqArrays = require("./eqArrays"); //  A function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// This function uses eqArrays to check if two arrays are equal and prints a message to the console.
 const assertArraysEqual = function(arr1, arr2) {
+
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
+
 };
 
-assertArraysEqual([], []);
-assertArraysEqual(["1", "2", "3"], []);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
