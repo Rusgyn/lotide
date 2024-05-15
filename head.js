6fca0f60c51d887a3dff1fa368fce56a9be7c799
rusgyn <rusgynmagsanay@gmail.com>
2024-05-15 08:42:57 -0400

    Modify line that checks if the given array is empty.

--- head.js
+++ head.js
@@ -9,15 +9,17 @@ const assertEqual = function(actual, expected) {
 };
 
 // FUNCTION IMPLEMENTATION: Function that retrieve the first element from the array, "head" of the array.
+
 const head = function(arr) {
-  if (!arr) {
+  if (arr.length === 0) { // This condition checks if given array is empty 
     return undefined;
   } else {
     return arr[0];
   }
-}
+};
 
 // TEST CODE
+
 assertEqual(head([5, 6, 7]), 5); // Test case output: Assertion Passed. Head is 5.
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");//Test case output: Assertion Passed. Head is "Hello".
 assertEqual(head([]), "Hello"); // Test case output: Assertion Failed. Array is undefined.
