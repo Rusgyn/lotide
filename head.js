c695c98f0a2bd83d453b69dad324fe0db7905107
rusgyn <rusgynmagsanay@gmail.com>
2024-05-15 08:37:15 -0400

    Implement function that retrieve the first element of an array

---
+++ head.js
@@ -0,0 +1,24 @@
+
+// FUNCTION THAT TEST CASES
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// FUNCTION IMPLEMENTATION: Function that retrieve the first element from the array, "head" of the array.
+const head = function(arr, element) {
+  if (!arr) {
+    return undefined;
+  } else {
+    return arr[0];
+  }
+}
+
+// TEST CODE
+assertEqual(head([5, 6, 7]), 5); // Test case output: Assertion Passed. Head is 5.
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");//Test case output: Assertion Passed. Head is "Hello".
+assertEqual(head([]), "Hello"); // Test case output: Assertion Failed. Array is undefined.
+assertEqual(head([3]), 3); //Test case output: Assertion Passed. Head is 3.
