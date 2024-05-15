c5242a8772e59612ce522a4f5bfabd2b88ab6693
rusgyn <rusgynmagsanay@gmail.com>
2024-05-15 09:10:05 -0400

    Implement function that returns the tail of a given array

---
+++ tail.js
@@ -0,0 +1,41 @@
+// FUNCTION THAT TEST CASES
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// FUNCTION IMPLEMENTATION: Function that returns a new array with the tail of the given array.
+
+const tail = function(arr) {
+  let newArray = [];
+
+  if (arr === undefined || arr.length <= 1) {
+    return newArray;
+  }
+
+  for (let i = 1; i < arr.length; i++) {
+    newArray.push(arr[i]);
+  }
+
+  return newArray;
+}
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case: Check an empty array
+const empty = [];
+tail(empty);
+assertEqual(empty.length, 0); // ensure we get back 0;
