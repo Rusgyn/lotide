b23150398ac1b7d0d6128d291175f129379db008
rusgyn <rusgynmagsanay@gmail.com>
2024-05-16 21:01:57 -0400

    Code that compare two arrays and console.log the result

---
+++ assertArraysEqual.js
@@ -0,0 +1,40 @@
+/*
+Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+*/
+
+// FUNCTION IMPLEMENTATION: Function that takes in two arrays and returns true or false, based on a perfect match.
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let [index, element] of arr1.entries()) {
+    if (element !== arr2[index]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// FUNCTION IMPLEMENTATION: Function that takes in two arrays and console.log an appropriate message to the console.
+
+const assertArraysEqual = function(array1, array2) {
+
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+
+};
+
+// TEST CODE:
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //✅✅✅ Assertion Passed: 1,2,3 === 1,2,3
+assertArraysEqual([1, 2, 4], [1, 2, 3]); //🛑🛑🛑 Assertion Failed: 1,2,4 !== 1,2,3
+assertArraysEqual([1, 2, 3], [3, 2, 1]); //🛑🛑🛑 Assertion Failed: 1,2,3 !== 3,2,1
+assertArraysEqual([1, 2, 3], "[1, 2, 3]"); //🛑🛑🛑 Assertion Failed: 1,2,3 !== [1, 2, 3]
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); //✅✅✅ Assertion Passed: 1,2,3 === 1,2,3
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); //🛑🛑🛑 Assertion Failed: 1,2,3 !== 1,2,3
