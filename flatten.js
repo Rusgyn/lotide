a962637e2b5b47940b7965a105c2e5b1c0a63020
rusgyn <rusgynmagsanay@gmail.com>
2024-05-17 10:20:24 -0400

    Modify the iteration of the arguments and add test case

--- flatten.js
+++ flatten.js
@@ -36,7 +36,7 @@ const assertArraysEqual = function(array1, array2) {
 
 const flatten = function(arr) {
 
-  let flatArr = [];
+  let flattenedArr = [];
 
   if (!Array.isArray(arr)) {
     console.log("Please provide an array argument");
@@ -46,15 +46,22 @@ const flatten = function(arr) {
   for (let element in arr) {
     if (Array.isArray(arr[element])) {
       for (let el in arr[element]) {
-        flatArr.push(arr[element][el]);
+        flattenedArr.push(arr[element][el]);
       }
     } else {
-    flatArr.push(arr[element]);
+    flattenedArr.push(arr[element]);
     }
   }
 
-  console.log(flatArr);
+  return flattenedArr;
 }
 
-flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
-flatten([[7, 8], [9, 10], 11, [12]]); // => [7, 8, 9, 10, 11, 12]
+//TEST CODE:
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+console.log(flatten([[7, 8], [9, 10], 11, [12]])); // => [7, 8, 9, 10, 11, 12]
+
+//TEST CASE:
+const numbers = [10.1, [10.01, -10.1], 10.6, [2, 4], 6];
+console.log(flatten(numbers)); //[10.1, 10.01, -10.1, 10.6, 2, 4, 6]
+// TEST CASE using assertArraysEqual
+assertArraysEqual(flatten(numbers), [10.1, 10.01, -10.1, 10.6, 2, 4, 6]);
