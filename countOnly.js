e8978f8e56216a62303131d30ab1d80b4c3cffe2
rusgyn <rusgynmagsanay@gmail.com>
2024-05-20 15:57:22 -0400

    Increment each item into results as we ecnounter each string item in the array

--- countOnly.js
+++ countOnly.js
@@ -14,7 +14,17 @@ const countOnly = function (allItems, itemsToCount) {
 
   for (const item of allItems) {
     console.log(item);
-   
+    
+    // inside the loop,
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+    
   }
 
   return results;
