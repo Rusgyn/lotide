c31302be54f5d7e6aa97d86998465391db74c395
rusgyn <rusgynmagsanay@gmail.com>
2024-05-20 15:55:56 -0400

    Add code to loop over all items in the array and print them to the console

--- countOnly.js
+++ countOnly.js
@@ -10,7 +10,14 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function (allItems, itemsToCount) {
- 
+  const results = {};
+
+  for (const item of allItems) {
+    console.log(item);
+   
+  }
+
+  return results;
 };
 
 const firstNames = [
