7ee83057eb05027b76f71268571be0e72b767cf8
rusgyn <rusgynmagsanay@gmail.com>
2024-05-20 15:53:36 -0400

    Initial function definition

---
+++ countOnly.js
@@ -0,0 +1,14 @@
+//HELPER FUNCTIONS:
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function (allItems, itemsToCount) {
+
+};
