90c18e7f1525928ea0f340c60e8d40474097286c
rusgyn <rusgynmagsanay@gmail.com>
2024-05-21 16:37:15 -0400

    Remove typo console.log

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -47,7 +47,7 @@ const assertObjectsEqual = function (actual, expected) {
   const inspect = require("util").inspect;
 
   if (eqObjects(actual, expected)) {
-    console.log(console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`));
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
