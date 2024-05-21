5da46a2c06a643611ea7e29a11f699bab92482d9
rusgyn <rusgynmagsanay@gmail.com>
2024-05-21 00:07:22 -0400

    Implement a letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,9 @@
+// FUNCTION IMPLEMENTATION
+// We'll implement a new function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
+//For each letter, instead of returning just one number to represent its number of occurrences, multiple numbers may be needed to represent all the places in the string that it shows up.
+
+const letterPositions = function (sentence) {
+  const results = {};
+  // logic to update results here
+  return results;
+};
