a431f3e32746929b1f121a59f04f4a75c011c97f
rusgyn <rusgynmagsanay@gmail.com>
2024-05-21 01:31:49 -0400

    Obtain all keys using object.keys method

---
+++ findKeyByValue.js
@@ -0,0 +1,16 @@
+
+// HELPER FUNCTIONS
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//FUNCTION IMPLEMENTATION:
+// Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
+const findKeyByValue = function(object, value) {
+  //obtain the key property of an object, returns an array.
+  let objectKey = Object.keys(object);
+};
