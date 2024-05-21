6a1955dfe8c13224051c94a7d9bb952fdf0ebb88
rusgyn <rusgynmagsanay@gmail.com>
2024-05-21 01:50:29 -0400

    Add test, primitive as  values

--- eqObjects.js
+++ eqObjects.js
@@ -11,4 +11,16 @@ const assertEqual = function(actual, expected) {
 //Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
   // Returns true if both objects have identical keys with identical values.
   // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {};
+const eqObjects = function (object1, object2) {
+
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject, anotherShirtObject); // => true
+//We need to use that return value in combination with assertEquals to test if the function is working correctly.
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject, longSleeveShirtObject); // => false
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
