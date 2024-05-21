8f9c160e931307bed688b30c402f1c3c418d5784
rusgyn <rusgynmagsanay@gmail.com>
2024-05-21 15:25:59 -0400

    Add helper function. Add code that check if value is an array or not

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,19 @@
+// HELPER FUNCTIONS: Function that takes in two arrays and returns true or false, based on a perfect match.
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let [index, element] of arr1.entries()) {
+    if (element !== arr2[index]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 // HELPER FUNCTIONS
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
@@ -9,22 +25,27 @@ const assertEqual = function(actual, expected) {
 
 //FUNCTION IMPLEMENTATIONS:
 //Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
-  // Returns true if both objects have identical keys with identical values.
-  // Otherwise you get back a big fat false!
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   let objectKey1 = Object.keys(object1); //returns an array with key elements
   let objectKey2 = Object.keys(object2); //returns an array with key elements
 
-  //Check if two given object has the same number of properties.
+  //Check if two given object has the same number of properties (length).
   if (objectKey1.length !== objectKey2.length) {
     return false;
   }
 
   for (let key1 of objectKey1) { //key1 = [color, size];
-    if(object1[key1] === object2[key1]) {
-          console.log("Line 31", object1[key1]);
-          console.log("Line 32:", object2[key1]);
-      return true;
+    //check if value matches, when the value is an array.
+    if (Array.isArray(object1[key1]) && Array.isArray(object2[key1])) {
+      if (!eqArrays(object1[key1], object2[key1])) {
+        return false;
+      }
+    } else { //check if value matches, when the value is "not" array.
+      if (object1[key1] !== object2[key1]) {
+        return false;
+      }
     }
   }
   
@@ -45,7 +66,7 @@ assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), false);
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
 
 const longSleeveMultiColorShirtObject = {
   size: "medium",
