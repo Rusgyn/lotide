b4d293aa03a9582e106927e9c8b72ee8372fe346
rusgyn <rusgynmagsanay@gmail.com>
2024-05-21 01:04:35 -0400

    Write test case

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,36 @@
+// HELPER IMPLEMENTATION: Function that takes in two arrays and returns true or false, based on a perfect match.
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let [index, element] of arr1.entries()) {
+    if (element !== arr2[index]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// HELPER FUNCTION: Function that takes in two arrays and console.log an appropriate message to the console.
+const assertArraysEqual = function(array1, array2) {
+
+  if (!Array.isArray(array1) && !Array.isArray(array2)) {
+    console.log("Please provide an array argument/s");
+    return;
+  }
+
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+
+};
+
+
 // FUNCTION IMPLEMENTATION
 // We'll implement a new function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
 //For each letter, instead of returning just one number to represent its number of occurrences, multiple numbers may be needed to represent all the places in the string that it shows up.
@@ -23,4 +56,17 @@ const letterPositions = function (sentence) {
   return results;
 };
 
+//TEST CODE
 console.log(letterPositions('lighthouse in the house'));
+
+//TEST CASE, using assertArrayEqual().
+assertArraysEqual(letterPositions("lighthouse in the house").l, [0]); //=> ✅✅✅ Assertion Passed: 0 === 0
+assertArraysEqual(letterPositions("lighthouse in the house").i, [1, 11]); //=> ✅✅✅ Assertion Passed: 1,11 === 1,11
+assertArraysEqual(letterPositions("lighthouse in the house").g, [2]); //=> ✅✅✅ Assertion Passed: 2 === 2
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]); //=> ✅✅✅ Assertion Passed: 3,5,15,18 === 3,5,15,18
+assertArraysEqual(letterPositions("lighthouse in the house").t, [4, 14]); //=> ✅✅✅ Assertion Passed: 4,14 === 4,14
+assertArraysEqual(letterPositions("lighthouse in the house").o, [6, 19]); //=> ✅✅✅ Assertion Passed: 6,19 === 6,19
+assertArraysEqual(letterPositions("lighthouse in the house").u, [7, 20]); //=> ✅✅✅ Assertion Passed: 7,20 === 7,20
+assertArraysEqual(letterPositions("lighthouse in the house").s, [8, 21]); //=> ✅✅✅ Assertion Passed: 8,21 === 8,21
+assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]); //=> ✅✅✅ Assertion Passed: 9,16,22 === 9,16,22
+assertArraysEqual(letterPositions("lighthouse in the house").n, [12]); //=> ✅✅✅ Assertion Passed: 12 === 12
