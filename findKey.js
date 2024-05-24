ecc8eccde57b549d5c0c4135b53176ee2e9f400e
rusgyn <rusgynmagsanay@gmail.com>
2024-05-24 15:11:25 -0400

    Define for...of method to loop the object

--- findKey.js
+++ findKey.js
@@ -9,8 +9,28 @@
  */
 
 // FUNCTION IMPLEMENTATION: Function findKey.
-const findKey = function(object, callback) {
+const findKey = function(object1, callback) {
+  
+  let result = undefined;
 
+  for (let objKey of object1) {
+
+  }
+
+  return result;
 };
+
+// TEST CASE;
+findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
 
 
