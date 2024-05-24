2d5ec46b71091bbfa87fac61f1cdf7859a388fc1
rusgyn <rusgynmagsanay@gmail.com>
2024-05-24 14:48:34 -0400

    Add test cases

--- takeUntil.js
+++ takeUntil.js
@@ -8,5 +8,16 @@
 //Keep collecting items from a provided array until the callback provided returns a truthy value.
 
 const takeUntil = function(array, callback) {
-  
+
 };
+
+// TEST CASE:
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
