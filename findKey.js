f37292330bfcd61892d9920f51fa57f3f156ccab
rusgyn <rusgynmagsanay@gmail.com>
2024-05-24 15:08:32 -0400

    Define function findKey definition.

---
+++ findKey.js
@@ -0,0 +1,16 @@
+/**Implement the function findKey which takes in an 
+ * INPUT:
+ * function findKey
+ * @param1 = object and a 
+ * @param2 = callback.
+ * OUTPUT:
+ * It should scan the object and return the first key for which the callback returns a truthy value. 
+ * If no key is found, then it should return undefined.
+ */
+
+// FUNCTION IMPLEMENTATION: Function findKey.
+const findKey = function(object, callback) {
+
+};
+
+
