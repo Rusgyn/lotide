36daa9893dabd2464c915752283b5820a72a1f06
rusgyn <rusgynmagsanay@gmail.com>
2024-06-06 15:10:20 -0400

    Refactor and modularize the code.

--- head.js
+++ head.js
@@ -1,26 +1,13 @@
+const assertEqual = require('./assertEqual');
 
-// FUNCTION THAT TEST CASES
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// FUNCTION IMPLEMENTATION: Function that retrieve the first element from the array, "head" of the array.
+// A function that retrieve the first element from the array, "head" of the array.
 
 const head = function(arr) {
-  if (arr.length === 0) { // This condition checks if given array is empty 
+  if (arr.length === 0) { // This condition checks if the given array is empty
     return undefined;
   } else {
     return arr[0];
   }
 };
 
-// TEST CODE
-
-assertEqual(head([5, 6, 7]), 5); // Test case output: Assertion Passed. Head is 5.
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");//Test case output: Assertion Passed. Head is "Hello".
-assertEqual(head([]), "Hello"); // Test case output: Assertion Failed. Array is undefined.
-assertEqual(head([3]), 3); //Test case output: Assertion Passed. Head is 3.
+module.exports = head;
