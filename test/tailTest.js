bfacd4623b022095bad9e02a2e16965068621e2d
rusgyn <rusgynmagsanay@gmail.com>
2024-06-07 00:19:17 -0400

    Refactor test code and use chai .deepEqual assertion

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,26 +1,33 @@
 // Test Code for tail()
 
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+//const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-//TEST CODE:
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// Test cases
+describe("#tail", function() {
+  it("should return the elements of the given array excluding the element at index 0", function() {
+    const result = tail(["Hello", "Lighthouse", "Labs"]); // => ["Lighthouse", "Labs"]
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("should return the elements of a given array excluding the element at index 0.", () => {
+    const test = tail([1, 2, 3]); //=> [2, 3];
+    assert.deepEqual(test[0], 2);
+  });
 
-// Test Case: Check an empty array
-const empty = [];
-const resultEmpty = tail(empty);
-assertEqual(resultEmpty.length, 0); // ensure we get back 0;
+  it("should return an empty array when the given array has one element only", () => {
+    const test = tail(["one"]); // => []
+    assert.deepEqual(tail(["one"]), []);
+  });
 
-// Test Case: Check an array with one element
-const single = ["only"];
-const resultSingle = tail(single);
-assertEqual(resultSingle.length, 0); // ensure we get back 0;
+  it("should return an empty array when the given array is empty", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+  it("should passed and return 3 as the number of length to the given array.", () => {
+    const words = tail(["Yo Yo", "Lighthouse", "Labs", "Cohort"]); // => ["Lighthouse", "Labs", "Cohort"]
+    assert.deepEqual(words.length, 3);
+  });
+
+});
