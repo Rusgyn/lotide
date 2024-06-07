b34c7e142ffcc0eb920aec9cf3d8bdd0252eacf7
rusgyn <rusgynmagsanay@gmail.com>
2024-06-07 00:15:55 -0400

    Implement chai strictEqual assertion

--- test/headTest.js
+++ test/headTest.js
@@ -4,24 +4,24 @@ const assert = require('chai').assert;
 const head = require('../head');
 
 describe("#head", () => {
-  it("returns 1 for [1, 2, 3]", () => {
+  it("returns the first element of an array. 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
 
-  it("returns '5' for ['5']", () => {
+  it("returns the element of an array with one element as its head, '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5'); 
   });
 
-  it("returns 5 for [5, 6, 7]", () => {
+  it("returns the first element of an array, 5 for [5, 6, 7]", () => {
     assert.strictEqual(head([5, 6, 7]), 5); 
   });
 
-  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+  it('returns the first element of an array, "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
   });
 
-  it('returns "Hello" for []', () => {
-    assert.notStrictEqual(head([]), "Hello");
-  });
+  // it('returns undefined for []', () => {
+  //   assert.strictEqual(head([]), undefined);
+  // });
 
 });
