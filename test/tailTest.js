9310f07196b5ce6fa00fd069428744533728fecc
rusgyn <rusgynmagsanay@gmail.com>
2024-06-07 00:33:48 -0400

    Modify two test codes to compare two arrays/objects

--- test/tailTest.js
+++ test/tailTest.js
@@ -13,12 +13,12 @@ describe("#tail", function() {
 
   it("should return the elements of a given array excluding the element at index 0.", () => {
     const test = tail([1, 2, 3]); //=> [2, 3];
-    assert.deepEqual(test[0], 2);
+    assert.deepEqual(test, [2, 3]);
   });
 
   it("should return an empty array when the given array has one element only", () => {
     const test = tail(["one"]); // => []
-    assert.deepEqual(tail(["one"]), []);
+    assert.deepEqual(test, []);
   });
 
   it("should return an empty array when the given array is empty", () => {
