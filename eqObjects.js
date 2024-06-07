4360c49ba9d3828b1acb977aa6b0d20294395c7f
rusgyn <rusgynmagsanay@gmail.com>
2024-06-07 14:33:44 -0400

    Import the function dependency, modularize the code, and separate the test cases

--- eqObjects.js
+++ eqObjects.js
@@ -1,32 +1,12 @@
-// HELPER FUNCTIONS: Function that takes in two arrays and returns true or false, based on a perfect match.
-const eqArrays = function(arr1, arr2) {
+// Imported function that takes in two arrays and returns true or false, based on a perfect match.
 
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
+const eqArrays = require('./eqArrays');
 
-  for (let [index, element] of arr1.entries()) {
-    if (element !== arr2[index]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-// HELPER FUNCTIONS. Compare "string" arguments.
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//FUNCTION IMPLEMENTATIONS:
-//Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
+/**
+ * Function which will take in two objects and returns true or false, based on a perfect match.
+ * Returns true if both objects have identical keys with identical values
+ * Otherwise you get back a big fat false!
+ */
 const eqObjects = function(object1, object2) {
   let objectKey1 = Object.keys(object1); //returns an array with key elements
   let objectKey2 = Object.keys(object2); //returns an array with key elements
@@ -52,27 +32,4 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-//TEST CASE
-const shirtObject = { color: "red", size: "medium"};
-const anotherShirtObject = { size: "medium", color: "red" };
-eqObjects(shirtObject, anotherShirtObject); // => true
-//We need to use that return value in combination with assertEquals to test if the function is working correctly.
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-eqObjects(shirtObject, longSleeveShirtObject); // => false
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
-
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
-
-const longSleeveMultiColorShirtObject = {
-  size: "medium",
-  colors: ["red", "blue"],
-  sleeveLength: "long",
-};
-eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
-assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
-
+module.exports = eqObjects;
