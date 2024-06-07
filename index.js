24b4ace87ec11fe145f51f1e0cb3f3b698e179dd
rusgyn <rusgynmagsanay@gmail.com>
2024-06-07 16:48:56 -0400

    Create object for all Lotide functions.

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqArrays = require('./eqArrays');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+
+module.exports = {
+
+  head, tail, middle, assertEqual, assertArraysEqual, assertObjectsEqual, countLetters, countOnly, eqArrays, eqObjects, findKey, findKeyByValue, flatten, letterPositions, takeUntil, without
+
+};
