42ea542d093d306bb55927259089dfa8da80982a
rusgyn <rusgynmagsanay@gmail.com>
2024-06-07 16:10:37 -0400

    Refactor, modularize the code.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,14 +1,8 @@
-// HELPER FUNCTIONS. Functions that compare "string".
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//FUNCTION IMPLEMENTATION:
-// Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
+/**
+ * Function which takes in an object and a value.
+ * It should scan the object return the first key which contains the given value.
+ * if no key with that given value is found then it should return undefined.
+ * */
 const findKeyByValue = function(object, value) {
   //obtain the key property of an object, returns an array.
   let objectKey = Object.keys(object);
@@ -22,17 +16,4 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
-// TEST CASE
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-};
-
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire")); // => drama
-console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show")); // => undefined
-console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine")); // => comedy
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); // => ✅✅✅ Assertion Passed: drama === drama
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);// ✅✅✅ Assertion Passed: undefined === undefined=>
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");// => ✅✅✅ Assertion Passed: comedy === comedy
+module.exports = findKeyByValue;
