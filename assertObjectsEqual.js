db78a565e7b11ae46699e5357a2409647feaa2e8
rusgyn <rusgynmagsanay@gmail.com>
2024-06-07 16:41:08 -0400

    Modularize the code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,47 +1,11 @@
-// HELPER FUNCTIONS: Function that takes in two arrays and returns true or false, based on a perfect match.
-const eqArrays = function(arr1, arr2) {
+// Function that takes in two arrays and returns true or false, based on a perfect match.
+const eqArrays = require('./eqArrays');
 
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
+//Compare two objects, return true if it matches else false.
+const eqObjects = require('./eqObjects');
 
-  for (let [index, element] of arr1.entries()) {
-    if (element !== arr2[index]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-//HELPER FUNCTIONS. Compare two objects, return true if it matches else false.
-const eqObjects = function(object1, object2) {
-  let objectKey1 = Object.keys(object1); //returns an array with key elements
-  let objectKey2 = Object.keys(object2); //returns an array with key elements
-
-  //Check if two given object has the same number of properties (length).
-  if (objectKey1.length !== objectKey2.length) {
-    return false;
-  }
-
-  for (let key1 of objectKey1) {
-    //check if value matches, when the value is an array.
-    if (Array.isArray(object1[key1]) && Array.isArray(object2[key1])) {
-      if (!eqArrays(object1[key1], object2[key1])) {
-        return false;
-      }
-    } else { //check if value matches, when the value is "not" array.
-      if (object1[key1] !== object2[key1]) {
-        return false;
-      }
-    }
-  }
-  
-  return true;
-};
-
-//FUNCTION IMPLEMENTATION: will take in two objects and console.log an appropriate message to the console.
-const assertObjectsEqual = function (actual, expected) {
+// Function that will take in two objects and console.log an appropriate message to the console.
+const assertObjectsEqual = function(actual, expected) {
 
   //built-in library function that is intended for debugging and returns a string representation of the object
   const inspect = require("util").inspect;
@@ -52,6 +16,8 @@ const assertObjectsEqual = function (actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
+
+module.exports = assertObjectsEqual;
 
 assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' }); // => ✅✅✅ Assertion Passed: { a: '1', b: 2 } === { b: 2, a: '1' }
 assertObjectsEqual({ a: 10, b: 2 }, { b: 2, a: '1' }); // => 🛑🛑🛑 Assertion Failed: { a: 10, b: 2 } !== { b: 2, a: '1' }
