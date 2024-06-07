55c7e9caaf8550d941e854df57533c0c06373baf
rusgyn <rusgynmagsanay@gmail.com>
2024-06-07 14:20:38 -0400

    Refactor and use chai assertions to test the functionality of eqArrays().

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,33 @@
 // Test Code for eqArrays()
 
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
 // TEST CODE:
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], "[1, 2, 3]"), false); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+describe("#eqArrays", function() {
+  it("should return true and passed for [], []", () => {
+    assert.strictEqual(assertEqual(eqArrays([], []), true));
+  }); // => ✅✅✅ Assertion Passed: true === true
+
+  it("should return false and passed for ['1', '2', '3'], []", () => {
+    assert.strictEqual(assertEqual(eqArrays(["1", "2", "3"], []), false));
+  }); // => ✅✅✅ Assertion Passed: false === false
+
+  it("should return true and passed for [1, 2, 3], [1, 2, 3]", () => {
+    assert.strictEqual(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+  }); // => ✅✅✅ Assertion Passed: true === true
+
+  it("should return false and passed for [1, 2, 3], [3, 2, 1", () => {
+    assert.strictEqual(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
+  }); // => ✅✅✅ Assertion Passed: false === false
+
+  it("should return true and passed for ['1', '2', '3'], ['1', '2', '3']", () => {
+    assert.strictEqual(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
+  }); // => ✅✅✅ Assertion Passed: true === true
+
+  it("should return false and passed for ['1', '2', '3'], ['1', '2', 3]", () => {
+    assert.strictEqual(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
+  }); // => ✅✅✅ Assertion Passed: false === false
+
+});
