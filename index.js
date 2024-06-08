97c8be9075016585bee1a846d353deff6aa2219b
rusgyn <rusgynmagsanay@gmail.com>
2024-06-07 22:28:09 -0400

    Organize, and add missing functions files.

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 // index.js
-const assertEqual = require('./assertEqual');
 const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
-const eqArrays = require('./eqArrays');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
+const head   = require('./head');
 const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const tail   = require('./tail');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
 
 module.exports = {
-
-  head,
-  tail,
-  middle,
-  assertEqual,
   assertArraysEqual,
+  assertEqual,
   assertObjectsEqual,
   countLetters,
   countOnly,
@@ -31,8 +28,11 @@ module.exports = {
   findKey,
   findKeyByValue,
   flatten,
+  head,
   letterPositions,
+  map,
+  middle,
+  tail,
   takeUntil,
   without
-
 };
--- map.js
+++ map.js
@@ -1,28 +1,9 @@
-// HELPER FUNCTION: Function that takes in two arrays and returns true or false, based on a perfect match.
-
-const eqArrays = require('./eqArrays');
-
-// HELPER FUNCTION: Function that takes in two arrays and console.log an appropriate message to the console.
-
-const assertArraysEqual = require('./assertArraysEqual');
-
-/**
- * Map function will take two arguments. An array to map, and a callback function
- * return new array based on the results of the callback function
- */
-
-const words = ["ground", "control", "to", "major", "tom"];
-
+//The map function will return a new array based on the results of the callback function.
 const map = function(array, callback) {
-
   const results = [];
-  // Iterate each elements in the given array using for...of
   for (let item of array) {
-    // The output of our callback function will push to our variable results
     results.push(callback(item));
   }
-
   return results; // The results will return a new array based on the results of our callback function.
 };
-
 module.exports = map;
