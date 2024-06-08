d4d3c2e6b7203b010aa63e443fe8e2674a68d9e0
rusgyn <rusgynmagsanay@gmail.com>
2024-06-08 19:16:42 -0400

    Remove extra spaces to be more cleaner.

--- countLetters.js
+++ countLetters.js
@@ -1,22 +1,16 @@
-//HELPER FUNCTIONS
-const assertEqual = require('./assertEqual');
-
 // Function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
+
 const countLetters = function(string) {
-  
   let countedLetter = {};
 
   for (let char of string) {
-
     // we can skip and not count spaces.
     if (char === ' ') {
       continue;
     }
-
     // increment the counter for each item:
     //   set a property with that string key to:
     //     the value that was already there (or zero if nothing there) with 1 added to it.
-
     if (countedLetter[char]) {
       countedLetter[char] += 1;
     } else {
