c0c46eb2534d86aacd8af2905a31061281d3da16
rusgyn <rusgynmagsanay@gmail.com>
2024-06-08 18:42:40 -0400

    Remove extra space and perform eslint.

--- map.js
+++ map.js
@@ -1,9 +1,10 @@
 //The map function will return a new array based on the results of the callback function.
-const map = function(array, callback) {
+const map = function (array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
   }
   return results; // The results will return a new array based on the results of our callback function.
 };
+
 module.exports = map;
